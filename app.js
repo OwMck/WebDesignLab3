9d46f9b97c86bda4b9a6ef2a4bc2d8e75134c7d5
OwMck <91203500+OwMck@users.noreply.github.com>
2021-11-22 18:59:22 +0000

    initial commit

--- app.js
+++ app.js
@@ -5,10 +5,24 @@ const app = express();
 
 //send an HTTP response when receiving HTTP GET /
 //send the index.html when receiving HTTP GET /
+//handling static HTML and EJS templates
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile("public/index.html", { root: __dirname });
+  res.render("index"); //no need for ejs extension
 });
-
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
+//route for sign-in page (login)
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+//route for register page
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
